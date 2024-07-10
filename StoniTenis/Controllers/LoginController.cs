fa9b4886f1356ac7c656b181e0381beec0ced09a
pejovicvuk <123560845+pejovicvuk@users.noreply.github.com>
2024-07-10 13:28:03 +0200

    Google Login() i Logout()

---
+++ StoniTenis/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace StoniTenis.Controllers
+{
+    public class LoginController : Controller  
+    {
+
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        public async Task LoginWithGoogle()
+        {
+            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+            {
+                RedirectUri = Url.Action("GoogleResponse")
+            });
+        }
+
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities
+                .FirstOrDefault().Claims.Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+            return Json(claims);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
